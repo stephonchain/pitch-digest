0d6f67b361daabfb90cf220e978541f0f6bda45d
stephonchain <76633572+stephonchain@users.noreply.github.com>
2025-08-05 17:22:45 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,9 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+  await auth();
+}
 });
 
 export const config = {
